c7fbf40c9ad50e5b6d078f3c1a76181f8d377620
ranjeetscience <ranjeetscience7150477@gmail.com>
2016-07-04 01:00:12 +0530

    Create happy coin.py

---
+++ happy coin.py
@@ -0,0 +1,17 @@
+t=input()
+while t>0:
+        t=t-1
+        n=input()
+        a=[0 for x in range(0,n+1)]
+        b=list
+        c=0
+        for i in range(0,n):
+                c=raw_input()
+                a[i]=c
+
+        if a.count('lxh')%2==0:
+                print 'hhb'
+        else:
+                print 'lxh'
+
+
